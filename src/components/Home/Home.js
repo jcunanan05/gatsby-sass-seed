27da73a35ac06b3b9666ee47dc219450526ba4f7
jcunanan05 <cunanan.jonathan1995@gmail.com>
2020-05-03 02:01:44 -0400

    Add home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CoreLayout from 'components/CoreLayout';
+
+export default function Home() {
+  return (
+    <CoreLayout>
+      <body className="home">
+        Hello World
+      </body>
+    </CoreLayout>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
-import React from "react"
+import Home from 'components/Home';
 
-export default () => <div>Hello world!</div>
+export default Home;
